ad94b31d0dab1128e4bfd62e405fe353833c4a3a
Dayniel <daynielbatista@gmail.com>
2020-06-09 00:31:47 +0100

    Finishing proyect

--- pin-login/pin-login.js
+++ pin-login/pin-login.js
@@ -5,9 +5,10 @@ class PinLogin {
             numPad: el.querySelector(".pin-login__numpad"),
             textDisplay: el.querySelector(".pin-login__text")
         };
+
         this.loginEndpoint = loginEndpoint;
         this.redirectTo = redirectTo;
-        this.maxNUmbers = maxNUmbers;
+        this.maxNumbers = maxNumbers;
         this.value = "";
 
         this._generatePad();
@@ -20,5 +21,58 @@ class PinLogin {
             "7", "8", "9",
             "backspace", "0", "done"
         ];
+
+        padLayout.forEach(key => {
+            const insertBreak = key.search(/[369]/) !== -1;
+            const keyEl = document.createElement("div");
+
+            keyEl.classList.add("pin-login__key");
+            keyEl.classList.toggle("material-icons", isNaN(key));
+            keyEl.textContent = key;
+            keyEl.addEventListener("click", () => {this._handleKeyPress(key)});
+            this.el.numPad.appendChild(document.createElement("br"));
+        });
+
     }
-}
+
+        _handleKeyPress(key) {
+            switch (key) {
+                case "backspace":
+                    this.value = this.value.substring(0, this.value.length -1);
+                    break;                    
+                case "done":
+                    this._attemptLogin();
+                    break;            
+                default:
+                    if (this.value.length < this.maxNumbers && !isNaN(key)) {
+                        this.value += key;
+                    }
+                    break; 
+            }
+            this._updateValueText();
+        }
+        
+        _updateValueText() {
+            this.el.textDisplay.value = "_".repeat(this.value.length);
+            this.el.textDisplay.classList.remove("pin-login__text--error");
+        }
+
+        _attemptLogin() {
+            if (this.value.length > 0) {
+                fetch(this.loginEndpoint, {
+                    method: "post",
+                    headers: {
+                        "Content-Type": "application/x-www-forum-urlencoded"
+                    },
+                    body: `pincode=${this.value}`
+                }).then(response => {
+                    if (response.status === 200) {
+                        window.location.href = this.redirectTo;
+                    } else {
+                        this.el.textDisplay.classList.add("pin-login__text--error");
+                    }
+                })
+            }
+        }
+
+    }
